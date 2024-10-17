293db876e35c5d34eb48482bcb9cb7880b24e720
NathanAzvdo <nathan.azevedo28@gmail.com>
2024-10-17 17:41:16 -0300

    fisrt commit

---
+++ src/main/java/com/Java10x/CadastrodeNinjas/CadastrodeNinjasApplication.java
@@ -0,0 +1,13 @@
+package com.Java10x.CadastrodeNinjas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastrodeNinjasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastrodeNinjasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Java10x/CadastrodeNinjas/CadastrodeNinjasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Java10x.CadastrodeNinjas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastrodeNinjasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
