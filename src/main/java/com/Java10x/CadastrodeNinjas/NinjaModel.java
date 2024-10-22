15e94f7812621365defbd4910b9c75165f7799ed
NathanAzvdo <nathan.azevedo28@gmail.com>
2024-10-21 21:15:25 -0300

    entidade criada

---
+++ src/main/java/com/Java10x/CadastrodeNinjas/NinjaModel.java
@@ -0,0 +1,47 @@
+package com.Java10x.CadastrodeNinjas;
+
+import jakarta.persistence.Entity;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+@Entity
+public class NinjaModel {
+
+    private Long NinjaID;
+    private String Nome;
+    private String email;
+    private int idade;
+
+    public NinjaModel(){
+
+    }
+
+    public NinjaModel(int idade, String email, String nome) {
+        this.idade = idade;
+        this.email = email;
+        Nome = nome;
+    }
+
+    public String getNome() {
+        return Nome;
+    }
+
+    public void setNome(String nome) {
+        Nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
