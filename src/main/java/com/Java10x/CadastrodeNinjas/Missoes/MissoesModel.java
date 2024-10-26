de6348d4c9a946ac0ac24474882c3f4b5830841f
NathanAzvdo <nathan.azevedo28@gmail.com>
2024-10-26 18:36:35 -0300

    feat: add lombok in the class for abstraction

--- src/main/java/com/Java10x/CadastrodeNinjas/Missoes/MissoesModel.java
+++ src/main/java/com/Java10x/CadastrodeNinjas/Missoes/MissoesModel.java
@@ -1,14 +1,26 @@
 package com.Java10x.CadastrodeNinjas.Missoes;
 
+import com.Java10x.CadastrodeNinjas.Ninjas.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Entity
 @Table(name="tb_missoes")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class MissoesModel {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String nome;
     private String dificuldade;
+
+    @OneToMany(mappedBy = "missoes")
+    private List<NinjaModel> ninjas;
 
 }
--- src/main/java/com/Java10x/CadastrodeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/com/Java10x/CadastrodeNinjas/Ninjas/NinjaModel.java
@@ -2,52 +2,30 @@ package com.Java10x.CadastrodeNinjas.Ninjas;
 
 import com.Java10x.CadastrodeNinjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 
 @Entity
 @Table(name="tb_cadastro")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class NinjaModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long NinjaID;
     private String Nome;
-    private String email;
     private int idade;
-    private List<MissoesModel> missoes;
 
-    public NinjaModel(){
+    @Column(unique = true)
+    private String email;
 
-    }
+    @ManyToOne
+    @JoinColumn(name="missoes_id")
+    private MissoesModel missoes;
 
-    public NinjaModel(int idade, String email, String nome) {
-        this.idade = idade;
-        this.email = email;
-        Nome = nome;
-    }
-
-    public String getNome() {
-        return Nome;
-    }
-
-    public void setNome(String nome) {
-        Nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
