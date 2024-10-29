23ca434ef7c5583d80f29f71fc2ec0d6f977ff93
NathanAzvdo <nathan.azevedo28@gmail.com>
2024-10-29 19:01:23 -0300

    feat: add missoes controller

--- src/main/java/com/Java10x/CadastrodeNinjas/Missoes/MissoesController.java
+++ src/main/java/com/Java10x/CadastrodeNinjas/Missoes/MissoesController.java
@@ -1,4 +1,28 @@
 package com.Java10x.CadastrodeNinjas.Missoes;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/missoes")
 public class MissoesController {
+
+    @GetMapping("/listar")
+    public String listarMissao(){
+        return "Missoes listadas";
+    }
+
+    @PostMapping("/criar")
+    public String criarMissao(){
+        return "missao criada";
+    }
+
+    @PutMapping("/alterar")
+    public String alterarMissao(){
+        return "Missao alterada";
+    }
+
+    @DeleteMapping("/deletar")
+    public String deletarMissao(){
+        return "Missao Deletada";
+    }
 }
