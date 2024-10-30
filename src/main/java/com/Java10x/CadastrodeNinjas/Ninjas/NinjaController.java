ba08c8e040ee50217d10e5fbffa711b9eca25ead
NathanAzvdo <nathan.azevedo28@gmail.com>
2024-10-29 21:44:47 -0300

    feat: get ninjas by id

--- src/main/java/com/Java10x/CadastrodeNinjas/Ninjas/NinjaController.java
+++ src/main/java/com/Java10x/CadastrodeNinjas/Ninjas/NinjaController.java
@@ -24,9 +24,9 @@ public class NinjaController {
         return ninjaService.listarNinjas();
     }
 
-    @GetMapping("/listarID")
-    public String listarTodosPorID(){
-        return "listar por ID";
+    @GetMapping("/listar/{id}")
+    public NinjaModel listarNinjasPorID(@PathVariable Long id){
+        return ninjaService.listarNinjasPorID(id);
     }
 
     @PutMapping("/alterarID")
--- src/main/java/com/Java10x/CadastrodeNinjas/Ninjas/NinjaService.java
+++ src/main/java/com/Java10x/CadastrodeNinjas/Ninjas/NinjaService.java
@@ -3,6 +3,7 @@ package com.Java10x.CadastrodeNinjas.Ninjas;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class NinjaService {
@@ -17,6 +18,10 @@ public class NinjaService {
         return ninjaRepository.findAll();
     }
 
+    public NinjaModel listarNinjasPorID(Long id){
+        Optional<NinjaModel> ninjaModel = ninjaRepository.findById(id);
+        return ninjaModel.orElse(null);
+    }
 
 
 }
